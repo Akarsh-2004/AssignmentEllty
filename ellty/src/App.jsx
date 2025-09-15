aaccfd1fc6d07ba6013e3a0b0992d547e80c24f5
Akarsh Saklani <Akarshsaklani222@gmail.com>
2025-09-15 19:57:22 +0530

    minor changes

--- ellty/src/App.jsx
+++ ellty/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 
-const pages = ["Page 1", "Page 2", "Page 3", "Page 4"];
+const pages = ["Page 1", "Page 2", "Page 3", "Page 4", "Page 5", "Page 6"];
 
 export default function App() {
   const [open, setOpen] = useState(false);
@@ -42,40 +42,43 @@ export default function App() {
 
       {open && (
         <div className="dropdown-menu">
-          <div className="dropdown-content">
-            {/* All Pages */}
-            <label className="label-with-checkbox">
-              <span>All pages</span>
-              <input
-                type="checkbox"
-                checked={isAllChecked}
-                onChange={handleAllChange}
-              />
-            </label>
-            <div className="divider" />
-
-            {/* Page list */}
-            {pages.map((page) => (
-              <label key={page} className="label-with-checkbox">
-                <span>{page}</span>
+          <div className="dropdown-content-wrapper">
+            <div className="dropdown-content">
+              {/* All Pages */}
+              <label className="label-with-checkbox all-pages-label">
+                <span>All pages</span>
                 <input
                   type="checkbox"
-                  checked={selected.includes(page)}
-                  onChange={() => handlePageChange(page)}
+                  checked={isAllChecked}
+                  onChange={handleAllChange}
                 />
               </label>
-            ))}
+              <div className="divider" />
 
-            <div className="divider" />
+              {/* Page list */}
+              <div className="pages-list">
+                {pages.map((page) => (
+                  <label key={page} className="label-with-checkbox">
+                    <span>{page}</span>
+                    <input
+                      type="checkbox"
+                      checked={selected.includes(page)}
+                      onChange={() => handlePageChange(page)}
+                    />
+                  </label>
+                ))}
+              </div>
+            </div>
           </div>
-
-          {/* Done button */}
-        <div>
-          <button className="done-btn" onClick={() => setOpen(false)}>
-            Done
-          </button>
+          
+          <div className="divider" />
+          
+          <div className="done-btn-container">
+            <button className="done-btn" onClick={() => setOpen(false)}>
+              Done
+            </button>
+          </div>
         </div>
-        </div>  
       )}
     </div>
   );
